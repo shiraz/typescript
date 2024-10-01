c5467a6969a0ee039ec4550de0d8fed6774bd1bd
shiraz <mirzasbaig@nyu.edu>
2024-09-30 21:22:16 -0400

    Completed section 2

--- section2/course-starting-project/app.ts
+++ section2/course-starting-project/app.ts
@@ -1,26 +1,15 @@
-type Combinable = number | string;
-type ConversionDescriptor = 'as-number' | 'as-text';
+let userInput: unknown;
+let userName: string;
 
-function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
-    let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
-        result = +input1 + +input2;
-    } else {
-        result = input1.toString() + input2.toString();
-    }
+userInput = 5;
+userInput = 'Test';
 
-    return result;
+if (typeof userInput === 'string') {
+    userName = userInput;
 }
 
-const combineAges = combine(30, 26, 'as-number');
-console.log(combineAges);
+function generateError(message: string, code: number): never {
+    throw { message, errorCode: code};
+}
 
-const combineStringAges = combine('40', '36', 'as-number');
-console.log(combineStringAges);
-
-const combineNames = combine('Wade', 'Wilson', 'as-text');
-console.log(combineNames);
-
-type User = { name: string } | string;
-let u1: User = {name: 'Max'};
-u1 = 'Michael';
+generateError('An error occurred!', 500);
---
+++ section2/course-starting-project/union-aliases.ts
@@ -0,0 +1,26 @@
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
+    let result;
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+        result = +input1 + +input2;
+    } else {
+        result = input1.toString() + input2.toString();
+    }
+
+    return result;
+}
+
+const combineAges = combine(30, 26, 'as-number');
+console.log(combineAges);
+
+const combineStringAges = combine('40', '36', 'as-number');
+console.log(combineStringAges);
+
+const combineNames = combine('Wade', 'Wilson', 'as-text');
+console.log(combineNames);
+
+type User = { name: string } | string;
+let u1: User = {name: 'Max'};
+u1 = 'Michael';
